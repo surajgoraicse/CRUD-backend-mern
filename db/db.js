bedba61ccf178b5d7b3d68eb8538bcba0b8a8769
surajgoraicse <surajgoraicse@gmail.com>
2025-01-13 18:07:47 +0530

    improved db connection code

--- db/db.js
+++ db/db.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
+import "dotenv/config"
+import {DB_NAME} from "../constants.js"
 
 const connectMongoDb = async (req, res) => {
     try {
-        const connect = await mongoose.connect(process.env.MONGODB_URI)
-        
+        const connect = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("Database connected successfully : " , connect.connection.host);
     } catch (error) {
-        throw 
+        console.error("Database connection error " , error.message) 
+        console.error("error object : " , error) 
+        process.exit(1)
     }
 }
